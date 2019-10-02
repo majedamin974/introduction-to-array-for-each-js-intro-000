cb969858b3f1981cc28f754be117ef8dd898ceed
Majed Amin <majedamin974@gmail.com>
2019-10-02 12:25:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
@@ -7,6 +6,7 @@ function doToElementsInArray(array, callback) {
 // Add your changeCompletely() function here:
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + `${array[index]}s!!!`;
+
   var animals = ["dog", "cat", "squirrel"];
   doToElementsInArray(animals, changeCompletely);
 }
